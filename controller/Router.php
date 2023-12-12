659fb41d837e77b3477bdc1ce5c61bc0444ab043
PETEL Noa <petel.noa@gmail.com>
2023-12-12 19:05:45 +0100

    azer
    a

--- controller/Router.php
+++ controller/Router.php
@@ -1,52 +1,36 @@
 <?php
 
 require_once 'WelcomeController.php';
-
-require_once 'BoissonsController.php';
-require_once 'FruitsController.php';
-require_once 'BiscuitsController.php';
-
+require_once 'ProductsController.php';
 class Router
 {
     private $twig;
     private $url;
     private $ctrlWelcome;
-    private $ctrlBoissons;
-    private $ctrlFruits;
-    private $ctrlBiscuits;
+    private $ctrlProducts;
     public function __construct($twig)
     {
         $this->twig = $twig;
         $this->ctrlWelcome = new WelcomeController();
-        $this->ctrlBoissons = new BoissonsController();
-        $this->ctrlFruits = new FruitsController();
-        $this->ctrlBiscuits = new BiscuitsController();
+        $this->ctrlProducts = new ProductsController();
     }
 
     public function route()
     {
         try {
             if (isset($_GET['page'])) {
-
+                
                 switch ($_GET['page']) {
                     case 'default':
                         $this->ctrlWelcome->render($this->twig);
-                        break;
-                    case 'boissons':
-                        $this->ctrlBoissons->render($this->twig);
-                        break;
-                    case 'biscuits':
-                        $this->ctrlBiscuits->render($this->twig);
-                        break;
-                    case 'fruits':
-                        $this->ctrlFruits->render($this->twig);
-                        break;
-
+                    case 'products':
+                        $this->ctrlProducts->render($this->twig);
                 }
             } else {
                 $this->ctrlWelcome->render($this->twig);
             }
 
+            
         } catch (Exception $e) {
             throw new Exception("Action invalide");
         }
