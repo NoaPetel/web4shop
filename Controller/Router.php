c1e26a4816eff020adbc0d74cee8607badbe8f43
PETEL Noa <petel.noa@gmail.com>
2023-12-12 13:04:17 +0100

    2eme save

---
+++ Controller/Router.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'WelcomeController.php';
+require_once 'ProductsController.php';
+class Router
+{
+    private $twig;
+    private $url;
+    private $ctrlWelcome;
+    private $ctrlProducts;
+    public function __construct($twig)
+    {
+        $this->twig = $twig;
+        $this->ctrlWelcome = new WelcomeController();
+        $this->ctrlProducts = new ProductsController();
+    }
+
+    public function route()
+    {
+        try {
+            if (isset($_GET['page'])) {
+                
+                switch ($_GET['page']) {
+                    case 'default':
+                        $this->ctrlWelcome->render($this->twig);
+                    case 'products':
+                        $this->ctrlProducts->render($this->twig);
+                }
+            } else {
+                $this->ctrlWelcome->render($this->twig);
+            }
+
+            
+        } catch (Exception $e) {
+            throw new Exception("Action invalide");
+        }
+    }
+
+
+}
